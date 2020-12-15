00931e819de7413db265a338a0b6362f4786e6d7
Mario Lopez <ez0212sp@icloud.com>
2020-12-14 20:14:23 -0500

    commit tree eb169171bdcd47f792184bf51fcc2f21b9b587b6

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,24 @@
-# Write a speak_to_grandma method.
+#  Write a speak_to_grandma method.
+#  Whatever you say to grandma, she should respond with
+#  HUH?! SPEAK UP, SONNY!
+#  unless you shout it (type in all capitals).
+#  If you shout, she can hear you (or at least she thinks so) 
+#  and yells back
+#  NO, NOT SINCE 1938!
+#  However if you say 'I LOVE YOU GRANDMA!', she should respond with
+#  'I LOVE YOU TOO PUMPKIN!'
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA")
 
-# NO, NOT SINCE 1938!
+    if phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase
+        return "NO, NOT SINCE 1938!"
+    else 
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+end
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+speak_to_grandma
+
---
+++ tempCodeRunnerFile.rb
@@ -0,0 +1,8 @@
+ef speak_to_grandma(phrase)
+    if phrase != phrase.upcase
+     return "HUH?! SPEAK UP, SONNY!"
+ ...     elsif phrase 
+      return "NO, NOT SINCE 1938!"
+    else phrase=="I LOVE YOU GRANDMA".upcase
+    end
+  end
